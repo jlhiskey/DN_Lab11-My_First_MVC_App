466afb36ca00d1ed3cf90a73f9186c38adfbe70c
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-23 09:56:02 -0800

    Complete HomeController.Result

--- DN_Lab11-My_First_MVC_App/DN_Lab11-My_First_MVC_App/Controllers/HomeController.cs
+++ DN_Lab11-My_First_MVC_App/DN_Lab11-My_First_MVC_App/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using DN_Lab11_My_First_MVC_App.Models;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,25 @@ namespace DN_Lab11_My_First_MVC_App.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int startYear, int endYear)
+        {
+            if(startYear <= endYear)
+            {
+                return RedirectToAction("Result", new { startYear, endYear });
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }           
+        }
+        
+        public IActionResult Result(int startYear, int endYear)
+        {
+            List<Person> filteredPeople = Person.Search(startYear, endYear);
+            return View(filteredPeople);
         }
     }
 }
