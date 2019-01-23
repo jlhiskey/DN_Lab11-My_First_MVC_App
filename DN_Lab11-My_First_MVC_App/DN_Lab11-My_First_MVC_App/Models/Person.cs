8e05c7a29dcf929c338b07d204485c3f1defeaf4
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-23 07:24:12 -0800

    Framed out initial person class.

---
+++ DN_Lab11-My_First_MVC_App/DN_Lab11-My_First_MVC_App/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DN_Lab11_My_First_MVC_App.Models
+{
+    public class Person
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Birth_Year { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+
+
+    }
+}
